333f082868bf770c98d4046d678b783487cd2000
M. David <david.majdandzic@inovatrend.com>
2022-06-10 16:20:33 +0200

    Implement dateParser

---
+++ app/src/main/java/com/example/cykashoppinglist/mapper/DateParser.java
@@ -0,0 +1,21 @@
+package com.example.cykashoppinglist.mapper;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DateParser {
+	public static Date parse(String input) {
+		String[] ts = input.split("T");
+		String ymd = ts[0];
+		String hms = ts[1];
+
+		int year = Integer.parseInt(ymd.split("-")[0]);
+		int month = Integer.parseInt(ymd.split("-")[1]);
+		int day = Integer.parseInt(ymd.split("-")[2]);
+		int hour = Integer.parseInt(hms.split(":")[0]);
+		int minute = Integer.parseInt(hms.split(":")[1]);
+		int second = Integer.parseInt(hms.split(":")[2].split("\\.")[0]);
+		return new Date(year, month, day, hour, minute, second);
+	}
+}
