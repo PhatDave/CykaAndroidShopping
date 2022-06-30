70ee6d7e41ddc84dbefc423b0351a030209cbf7b
M. David <david.majdandzic@inovatrend.com>
2022-06-30 21:53:30 +0200

    Implement adapter

---
+++ app/src/main/java/com/example/cykashoppinglist/adapter/Adapter.java
@@ -0,0 +1,66 @@
+package com.example.cykashoppinglist.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cykashoppinglist.R;
+import com.example.cykashoppinglist.entity.Item;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+	LayoutInflater inflater;
+	List<Item> items;
+
+	public Adapter(Context ctx, List<Item> songs) {
+		this.inflater = LayoutInflater.from(ctx);
+		this.items = songs;
+	}
+
+
+	@NonNull
+	@Override
+	public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+		View view = inflater.inflate(R.layout.item, parent, false);
+		return new ViewHolder(view);
+	}
+
+	@Override
+	public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+		holder.itemName.setText(items.get(position).getName());
+		holder.itemDate.setText(items.get(position).getDate().toString());
+
+	}
+
+	@Override
+	public int getItemCount() {
+		return items.size();
+	}
+
+	public class ViewHolder extends RecyclerView.ViewHolder {
+		TextView itemName, itemDate;
+
+		public ViewHolder(@NonNull View itemView) {
+			super(itemView);
+
+			itemName = itemView.findViewById(R.id.itemName);
+			itemDate = itemView.findViewById(R.id.itemDate);
+
+			itemView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					// todo implement
+					Toast.makeText(v.getContext(), "Do Something With this Click", Toast.LENGTH_SHORT).show();
+				}
+			});
+		}
+	}
+}
